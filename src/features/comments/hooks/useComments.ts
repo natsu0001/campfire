eb76517df53cfbd39be30afb9d7cd768a05131a5
natsu0001 <talukdarabhishek687@gmail.com>
2026-07-07 15:02:48 +0530

    comments hooks

---
+++ src/features/comments/hooks/useComments.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { commentService } from "@/services/comment.service";
+
+export function useComments(postId: string) {
+  return useQuery({
+    queryKey: ["comments", postId],
+    queryFn: () => commentService.getComments(postId),
+  });
+}
---
+++ src/features/comments/hooks/useCreateComment.ts
@@ -0,0 +1,33 @@
+import {
+    useMutation,
+    useQueryClient,
+} from "@tanstack/react-query";
+
+import { commentService } from "@/services/comment.service";
+
+export function useCreateComment() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      postId,
+      authorId,
+      content,
+    }: {
+      postId: string;
+      authorId: string;
+      content: string;
+    }) =>
+      commentService.createComment(
+        postId,
+        authorId,
+        content
+      ),
+
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ["comments", variables.postId],
+      });
+    },
+  });
+}
